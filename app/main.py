9446ef41d11c0420e5e78ae187702ad334ccab01
MdJafirAshraf <mdjafirashraf@gmail.com>
2026-03-20 00:59:05 +0530

    integrate to mcp

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastmcp import FastMCP
 
 from app.api.v1.api import router as api_router
 from app.core.config import settings
@@ -9,23 +10,28 @@ from app.db.seed import seed_database
 from app.db.session import Base, engine
 
 
-def create_app() -> FastAPI:
-    app = FastAPI(title=settings.app_name, version=settings.app_version)
-
-    # Mount static files from the app/static directory.
-    app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-    app.include_router(api_router)
-
-    return app
-
-
-app = create_app()
-
-
 # Create database tables & seed on startup
 Base.metadata.create_all(bind=engine)
 seed_database()
+
+# Initialize FastAPI app with metadata.
+app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+# Mount static files from the app/static directory.
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Include API routes.
+app.include_router(api_router)
+
+
+# Convert to MCP server
+mcp = FastMCP.from_fastapi(app=app) 
+mcp_app = mcp.http_app(path='/mcp') # Create ASGI app from MCP server
+
+# Mount MCP app to FastAPI app
+app.mount("/llm", mcp_app)
+
+print("✅ MCP Server mounted at /mcp")
 
 
 if __name__ == "__main__":
